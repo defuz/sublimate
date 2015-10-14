f11762c645344dc3460c026be8cee9fdcb8cda46
defuz <defuz.net@gmail.com>
2015-10-14 19:19:29 +0300

    updated context

--- src/core/context.rs
+++ src/core/context.rs
@@ -1,45 +1,92 @@
-enum Operand {
-    /// Returns `true` if the autocomplete list is visible.
-    AutoCompleteVisible,
-    /// Returns `true` if a next snippet field is available.
-    HasNextField,
-    /// Returns `true` if a previous snippet field is available.
-    HasPrevField,
-    /// Returns `true` if any overlay is visible.
-    OverlayVisible,
-    /// Returns `true` if any panel is visible.
-    PanelVisible,
-    /// Returns `true` if the selection is an empty region.
-    SelectionEmpty,
-    /// Returns `true` if a panel has input focus.
-    PanelHasFocus,
-    /// Returns `true` if the panel given as operand is visible.
-    Panel,
-    /// Returns the number of selections.
-    NumSelections,
-    /// Returns the name of the current scope.
-    Selector,
-    /// Restricts the test to the text following the caret.
-    FollowingText,
-    /// Restricts the test to the text preceding the caret.
-    PrecedingText,
-    /// Restricts the test to the selected text.
-    Text,
-    /// Returns the value of the setting.
-    Setting(String)
-}
+use std::convert::From;
+use rustc_serialize::json::Json;
+use ParseContextRuleError::*;
 
-enum Operator {
+enum Operator<T> {
     /// Test for equality.
-    Equal, NotEqual,
+    Equal(T), NotEqual(T),
     /// Match against a regular expression (full match).
-    RegexMatch, NotRegexMatch,
+    RegexMatch(String), NotRegexMatch(String),
     /// Match against a regular expression (partial match).
-    RegexContains, NotRegexContains
+    RegexContains(String), NotRegexContains(String)
 }
 
-struct ContextRule {
+enum ContextRule {
+    /// Returns `true` if the autocomplete list is visible.
+    AutoCompleteVisibleEqual(bool),
+    /// Returns `true` if a next snippet field is available.
+    HasNextFieldEqual(bool),
+    /// Returns `true` if a previous snippet field is available.
+    HasPrevFieldEqual(bool),
+    /// Returns `true` if any overlay is visible.
+    OverlayVisibleEqual(bool),
+    /// Returns `true` if any panel is visible.
+    PanelVisibleEqual(bool),
+    /// Returns `true` if a panel has input focus.
+    PanelHasFocusEqual(bool),
+    /// Returns `true` if the panel given as operand is visible.
+    PanelEqual(bool),
+    /// Returns `true` if the selection is an empty region.
+    SelectionEmptyEqual(bool, /* match_all = */ bool),
+    /// Returns the number of selections.
+    NumSelectionsEqual(uint),
+    /// Returns the number of selections.
+    NumSelectionsNotEqual(uint),
+    /// Returns the name of the current scope.
+    Selector(Operator<String>),
+    /// Restricts the test to the text following the caret.
+    FollowingText(Operator<String>),
+    /// Restricts the test to the text preceding the caret.
+    PrecedingText(Operator<String>),
+    /// Restricts the test to the selected text.
+    Text(Operator<String>),
+    /// Returns the value of the setting.
+    Setting(String, Operator<Settings>)
+}
 
+enum ParseContextRuleError {
+    NotObject,
+    KeyNameNotFound,
+    UnknownKey,
+    IncorrectOperatorForBooleanKey
+}
+
+impl From<Json> for ContextRule {
+    fn from_settings(s: Settings) -> Result<Key, Self::Err> {
+        if let Settings::Object(obj) = s {
+            match obj.remove("key") {
+                Some(Settings::String(key)) => {
+                    if key.starts_with("setting.") {
+                        let key =
+                    } else {
+                        let rule_builder = match key {
+                            "auto_complete_visible" => ContextRule::AutoCompleteVisibleEqual,
+                            "has_next_field"        => ContextRule::HasNextFieldEqual,
+                            "has_prev_field"        => ContextRule::HasPrevFieldEqual,
+                            "overlay_visible"       => ContextRule::OverlayVisibleEqual,
+                            "panel_visible"         => ContextRule::PanelVisibleEqual,
+                            "panel_has_focus"       => ContextRule::PanelHasFocusEqual,
+                            "panel"                 => ContextRule::PanelEqual,
+                            _                       => return Err(UnknownKey)
+                        };
+                        match (obj.remove("operator"), obj.remove("operand")) {
+                            (Some(Settings::String("not_equal")), Some(Settings::Bool(false))) |
+                            (Some(Settings::String("equal")), Some(Settings::Bool(true)))      |
+                            (None, None)
+                                => rule_builder(true),
+                            (Some(Settings::String("not_equal")), Some(Settings::Bool(true)))  |
+                            (Some(Settings::String("equal")), Some(Settings::Bool(false)))
+                                => rule_builder(false),
+                            _   => return Err(IncorrectOperatorForBooleanKey)
+                        }
+                    }
+                },
+                _ => Err(KeyNameNotFound)
+            }
+        } else {
+            Err(NotObject)
+        }
+    }
 }
 
 struct Context {
--- src/core/settings.rs
+++ src/core/settings.rs
@@ -1,1 +1,19 @@
 pub use rustc_serialize::json::Json as Settings;
+pub use rustc_serialize::json::JsonArray as SettingsArray;
+pub use rustc_serialize::json::JsonObject as SettingsObject;
+
+trait FromSettings {
+    fn from_settings(settings: Settings) -> Self;
+}
+
+trait TryFromSettings {
+    type Error;
+    fn try_from_settings(settings: Settings) -> Result<Self, Self::Error>
+}
+
+impl TryFromSettings for T where T: FromSettings {
+    type Error = ();
+    fn try_from_settings(settings: Settings) -> Result<Self, Self::Error> {
+        Ok(Self::from_settings(settings))
+    }
+}
