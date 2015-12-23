87450ef7ad3efce31544c5458355c7621854e49d
defuz <defuz.net@gmail.com>
2015-12-23 20:25:28 +0200

    refactoring settings.rs and parsing of plist

--- src/core/packages.rs
+++ src/core/packages.rs
@@ -2,11 +2,7 @@ use std::io::{Read, Error as IoError, BufReader};
 use std::fs::File;
 use std::path::{Path, PathBuf};
 
-use weakjson;
-use rustc_serialize::json::ParserError;
-use plist::xml::StreamingParser;
-
-use core::settings::{Settings, FromSettings, FromPlist, ParsePlist, Plist};
+use core::settings::{Settings, SettingsError, read_json, read_plist, FromSettings, ParseSettings};
 use core::menu::Menu;
 use core::keymap::Keymap;
 use core::color_scheme::ColorScheme;
@@ -17,13 +13,13 @@ pub struct PackageRepository {
 }
 
 enum PackageError {
-    ParseSettings(ParserError),
+    Settings(SettingsError),
     Io(IoError)
 }
 
-impl From<ParserError> for PackageError {
-    fn from(error: ParserError) -> PackageError {
-        PackageError::ParseSettings(error)
+impl From<SettingsError> for PackageError {
+    fn from(error: SettingsError) -> PackageError {
+        PackageError::Settings(error)
     }
 }
 
@@ -43,25 +39,23 @@ impl PackageRepository {
         Ok(BufReader::new(reader))
     }
 
-    pub fn read_settings(&self, path: &Path) -> Result<Settings, PackageError> {
-        let mut reader = try!(self.read_file(path));
-        Ok(try!(weakjson::from_reader(&mut reader as &mut Read)))
+    pub fn read_json(&self, path: &Path) -> Result<Settings, PackageError> {
+        Ok(try!(read_json(try!(self.read_file(path)))))
     }
 
-    pub fn read_plist(&self, path: &Path) -> Result<Plist, PackageError> {
-        let reader = try!(self.read_file(path));
-        Ok(Plist::new(reader))
+    pub fn read_plist(&self, path: &Path) -> Result<Settings, PackageError> {
+        Ok(try!(read_plist(try!(self.read_file(path)))))
     }
 
     pub fn get_menu<P: AsRef<Path>>(&self, path: P) -> Menu {
-        match self.read_settings(path.as_ref()) {
+        match self.read_json(path.as_ref()) {
             Ok(settings) => Menu::from_settings(settings),
             Err(..) => Menu::default()
         }
     }
 
     pub fn get_keymap<P: AsRef<Path>>(&self, path: P) -> Keymap {
-        match self.read_settings(path.as_ref()) {
+        match self.read_json(path.as_ref()) {
             Ok(settings) => Keymap::from_settings(settings),
             Err(..) => Keymap::default()
         }
@@ -69,7 +63,7 @@ impl PackageRepository {
 
     pub fn get_color_scheme<P: AsRef<Path>>(&self, path: P) -> ColorScheme {
         match self.read_plist(path.as_ref()) {
-            Ok(ref mut plist) => match ColorScheme::parse_plist(plist) {
+            Ok(settings) => match ColorScheme::parse_settings(settings) {
                 Ok(color_scheme) => color_scheme,
                 Err(..) => ColorScheme::default()
             },
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+#![feature(slice_patterns)]
 
 mod core;
 mod toolkit;
